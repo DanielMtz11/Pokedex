36058816bb167f613b90ecf2746e9d60df316201
Daniel Martinez <daniel.martinez.b11@gmail.com>
2022-04-06 21:44:12 -0500

    styles to pokedeData

--- src/Components/PokedexData.js
+++ src/Components/PokedexData.js
@@ -20,10 +20,11 @@ const PokedexData = () => {
     },[Name])
 
     const type = pokemon?.types?.[0].type?.name;
+    // const type2 = pokemon?.types?.[1].type?.name;
 
     
     const style ={
-        background: type==='normal'?
+        background: type==='normal'  ?
                 (
                     "linear-gradient(212deg, rgba(27,10,9,0.6867121848739496) 10%, rgba(255,239,147,1) 99%)"
                 ):(    
@@ -36,7 +37,7 @@ const PokedexData = () => {
                         (
                             "green"
                         ):(
-                            type === 'poison'?(
+                            type === 'poison' ?(
                                 "linear-gradient(293deg, rgba(46,29,55,1) 0%, rgba(165,56,184,0.908000700280112) 100%) "
                             ):(
                                 type === 'fire'?(
@@ -68,7 +69,7 @@ const PokedexData = () => {
                                                             "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(58,131,175,1) 91%)"
                                                             
                                                         ):(
-                                                            type  === 'grass'?(
+                                                            type  === 'grass' ?(
                                                                 "linear-gradient(156deg, rgba(32,89,125,0.9304096638655462) 19%, rgba(15,245,184,0.6951155462184874) 83%)"
                                                             ):(
                                                                 type === 'electric'?(
@@ -143,10 +144,56 @@ const PokedexData = () => {
                 </div>
                     <h2>{pokemon.name}</h2>
 
+                <div className='container-types'>
                     {pokemon.types?.map(type =>(
-                        <h3 key={type.type.url}>{type.type.name}</h3>
+                        
+                        <h3 style={{background:type.type.name === 'grass'?( "linear-gradient(156deg, rgba(32,89,125,0.9304096638655462) 19%, rgba(15,245,184,0.6951155462184874) 83%)"):(
+                                    type.type.name === 'poison'?("linear-gradient(293deg, rgba(46,29,55,1) 0%, rgba(165,56,184,0.908000700280112) 100%) "):(
+                                        type.type.name === 'normal'?( "linear-gradient(212deg, rgba(27,10,9,0.6867121848739496) 10%, rgba(255,239,147,1) 99%)"):(
+                                            type.type.name === 'fighting'?("linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(74,30,13,1) 0%, rgba(187,65,24,0.908000700280112) 85%)"):(
+                                                type.type.name === 'flying'?("linear-gradient(135deg, rgba(215,44,79,1) 19%, rgba(0,38,223,0.6951155462184874) 83%)"):(
+                                                    type.type.name === 'fire'?( "linear-gradient(212deg, rgba(253,29,29,1) 9%, rgba(252,176,69,1) 100%)"):(
+                                                        type.type.name === 'ground'?("linear-gradient(0deg, rgba(168,135,41,1) 0%, rgba(55,43,9,0.908000700280112) 83%)"):(
+                                                            type.type.name ==='rock'?("linear-gradient(0deg, rgba(10,10,10,1) 0%, rgba(156,156,156,1) 84%, rgba(185,185,185,1) 99%)"):(
+                                                                type.type.name === 'bug'?("linear-gradient(90deg, rgba(37,68,34,1) 0%, rgba(88,184,56,0.908000700280112) 100%)"):(
+                                                                    type.type.name === 'ghost'?("linear-gradient(0deg, rgba(12,35,230,0.8155637254901961) 12%, rgba(31,1,50,1) 99%)"):(
+                                                                        type.type.name === 'steel'?(" linear-gradient(57deg, rgba(171,189,204,1) 15%, rgba(24,49,65,1) 97%)"):(
+                                                                            type.type.name === 'water'?("linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(58,131,175,1) 91%)"):(
+                                                                                type.type.name === 'electric'?("linear-gradient(333deg, rgba(12,37,59,1) 15%, rgba(19,131,204,1) 97%)"):(
+                                                                                    type.type.name === 'psychic'?("linear-gradient(63deg, rgba(247,231,147,0.9304096638655462) 19%, rgba(116,86,0,1) 97%)"):(
+                                                                                        type.type.name === 'ice'?("linear-gradient(63deg, rgba(11,56,84,0.9304096638655462) 19%, rgba(0,116,85,1) 97%)"):(
+                                                                                            type.type.name === 'dragon'?("linear-gradient(179.75deg, #478A93 -19.96%, #56A4AE 43.67%, #A2BEC1 138.4%)"):(
+                                                                                                type.type.name === 'dark'?("linear-gradient(71deg, rgba(111,111,111,1) 16%, rgba(101,104,106,1) 19%, rgba(0,0,0,1) 97%)"):(
+                                                                                                    type.type.name === 'fairy'?(" linear-gradient(179.75deg, #971B45 -19.96%, #C23867 43.67%, #CD7D98 138.4%)"):(
+                                                                                                        "linear-gradient(177.03deg, #030706 -11.97%, #0D1211 57.49%, #5A5E5D 135.64%)"
+                                                                                                    )
+                                                                                                )
+                                                                                            )
+                                                                                        )
+                                                                                    )
+                                                                                )
+                                                                            )
+                                                                        )
+                                                                    )
+                                                                )
+                                                            )
+                                                        )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        }}
+                        
+                        key={type.type.url}>{type.type.name}</h3>
                         // console.log(type.type.name)
-                    ))}
+                        ))}
+                </div>
+
+
+                    <p>height: {pokemon.height/10}</p>
+                    <p>weight: {pokemon.weight/10}</p>
 
                     <section className='content-pokedexData-abilities'>
                         abilities:
@@ -155,31 +202,6 @@ const PokedexData = () => {
 
                     </section>
 
-                    <p>height: {pokemon.height}</p>
-                    <p>weight: {pokemon.weight}</p>
-
-
-                    {/* <div>types:
-                    <p> {pokemon?.types?.[0].type?.name}</p>
-                    <p> {pokemon?.types?.[1].type?.name}</p>
-                        
-                    </div> */}
-
-                    {/* <div>Movements
-                        <ul>
-
-                        {
-                            pokemon.moves?.map(move=>
-                                <li key={move?.move?.name}>
-                                    {move?.move?.name}
-                                </li>
-                                // console.log(move.move.name)
-                                // console.log(move?.name)
-                                )
-                            }
-
-                            </ul>
-                    </div> */}
                                 </section>
 
 
